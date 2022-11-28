f9c593b77bd3f50ebc903f6c747d1a426aa24c87
yuki <angelsflyinhell@namespace.media>
2022-11-28 12:39:01 +0100

    Create UI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,34 @@
 <script setup lang="ts">
+import FieldVue from './components/Field.vue';
+
+const id = "232"
 </script>
 
 <template>
-  <h1 class="font-bold">Hello World</h1>
-  <p>Lorem Ipsum</p>
+  <div class="flex flex-col text-center w-screen bg-primary h-screen p-8">
+    <h1 class="text-4xl font-bold text-white top-0 p-4">ID: {{ id }}</h1>
+    <div class="flex flex-col h-5/6 max-w-md">
+      <div class="flex flex-row h-1/4 p-8">
+        <FieldVue id=0 :active="false" />
+        <div class="h-full w-1 bg-white" />
+        <FieldVue id=1 :active="false" />
+      </div>
+      <div class="flex flex-row h-1/4 p-8">
+        <FieldVue id=2 :active="true" />
+        <div class="h-full w-1 bg-white" />
+        <FieldVue id=3 :active="false" />
+      </div>
+      <div class="flex flex-row h-1/4 p-8">
+        <FieldVue id=4 :active="false" />
+        <div class="h-full w-1 bg-white" />
+        <FieldVue id=5 :active="false" />
+      </div>
+      <div class="flex flex-row h-1/4 p-8">
+        <FieldVue id=6 :active="false" />
+        <div class="h-full w-1 bg-white" />
+        <FieldVue id=7 :active="false" />
+      </div>
+    </div>
+    <p class="text-white font-bold underline"><a href="https://github.com/angelsflyinhell/clash">GitHub</a></p>
+  </div>
 </template>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,vue}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#7F669D",
+        secondary: "#BA94D1",
+        accent: "#DEBACE",
+        error: "#FBFACD"
+      },
+    },
   },
   plugins: [],
-}
+};
