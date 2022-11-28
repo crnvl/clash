485025c83919124b6b2eba775b869194708d9a75
angelsflyinhell <azami@namespace.media>
2022-11-28 17:10:00 +0100

    Fix positioning on large screens

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,31 +2,36 @@
 import FieldVue from './components/Field.vue';
 
 const id = "232"
+const field = 5;
+
+const fieldData: boolean[] = new Array(9).fill(false);
+fieldData[field] = true;
+
 </script>
 
 <template>
-  <div class="flex flex-col text-center w-screen bg-primary h-screen p-8">
+  <div class="flex flex-col items-center text-center w-screen bg-primary h-screen p-8">
     <h1 class="text-4xl font-bold text-white top-0 p-4">ID: {{ id }}</h1>
-    <div class="flex flex-col h-5/6 max-w-md">
-      <div class="flex flex-row h-1/4 p-8">
-        <FieldVue id=0 :active="false" />
+    <div class="flex flex-col h-5/6">
+      <div class="flex flex-row h-1/4 p-8 w-screen">
+        <FieldVue :active="fieldData[0]" />
         <div class="h-full w-1 bg-white" />
-        <FieldVue id=1 :active="false" />
+        <FieldVue :active="fieldData[1]" />
       </div>
       <div class="flex flex-row h-1/4 p-8">
-        <FieldVue id=2 :active="true" />
+        <FieldVue :active="fieldData[2]" />
         <div class="h-full w-1 bg-white" />
-        <FieldVue id=3 :active="false" />
+        <FieldVue :active="fieldData[3]" />
       </div>
       <div class="flex flex-row h-1/4 p-8">
-        <FieldVue id=4 :active="false" />
+        <FieldVue :active="fieldData[4]" />
         <div class="h-full w-1 bg-white" />
-        <FieldVue id=5 :active="false" />
+        <FieldVue :active="fieldData[5]" />
       </div>
       <div class="flex flex-row h-1/4 p-8">
-        <FieldVue id=6 :active="false" />
+        <FieldVue :active="fieldData[6]" />
         <div class="h-full w-1 bg-white" />
-        <FieldVue id=7 :active="false" />
+        <FieldVue :active="fieldData[7]" />
       </div>
     </div>
     <p class="text-white font-bold underline"><a href="https://github.com/angelsflyinhell/clash">GitHub</a></p>
