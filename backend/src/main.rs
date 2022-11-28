0967262836079a68c75408054b1ffdfd731ae79d
angelsflyinhell <azami@namespace.media>
2022-11-28 21:34:07 +0100

    Update main.rs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,18 @@
 extern crate redis;
+use redis::{Client, RedisError, Commands, RedisResult};
 
-fn main() {
+fn main() -> Result<(), RedisError> {
     // connect to redis
-    let client = redis::Client::open("redis://qwq.sh/");
-    let mut con = client.get_connection().expect();
+    let client = redis::Client::open("redis://qwq.sh:6379")?;
+    let mut con = client.get_connection()?;
+
     // throw away the result, just make sure it does not fail
-    let _: () = con.set("my_key", 42);
+    let _: () = con.set("my_key", 42)?;
     // read back the key and return it.  Because the return value
     // from the function is a result for integer this will automatically
     // convert into one.
-    con.get("my_key");
-    print!("{}", con.get("my_key"));
+    let value: i32 = con.get("my_key")?;
+    print!("{}", value);
+    
+    Ok(())
 }
