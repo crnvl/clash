1368dae840a05c9f762579c64e6e4b7c0ba52f19
angelsflyinhell <azami@namespace.media>
2022-12-06 15:55:31 +0100

    Things

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import FieldVue from './components/Field.vue';
+
+const id = "232"
+const field = 0;
+
+const fieldData: boolean[] = new Array(9).fill(false);
+fieldData[field] = true;
+
+</script>
+
+<template>
+  <h1 class="text-4xl font-bold text-white top-0 p-4">ID: {{ id }}</h1>
+  <div class="flex flex-col h-5/6">
+    <div class="flex flex-row h-1/4 p-8 w-screen">
+      <FieldVue :active="fieldData[0]" />
+      <div class="h-full w-1 bg-white" />
+      <FieldVue :active="fieldData[1]" />
+    </div>
+    <div class="flex flex-row h-1/4 p-8">
+      <FieldVue :active="fieldData[2]" />
+      <div class="h-full w-1 bg-white" />
+      <FieldVue :active="fieldData[3]" />
+    </div>
+    <div class="flex flex-row h-1/4 p-8">
+      <FieldVue :active="fieldData[4]" />
+      <div class="h-full w-1 bg-white" />
+      <FieldVue :active="fieldData[5]" />
+    </div>
+    <div class="flex flex-row h-1/4 p-8">
+      <FieldVue :active="fieldData[6]" />
+      <div class="h-full w-1 bg-white" />
+      <FieldVue :active="fieldData[7]" />
+    </div>
+  </div>
+  <p class="text-white font-bold underline"><a href="https://github.com/angelsflyinhell/clash">GitHub</a></p>
+</template>
---
+++ frontend/src/utils/api.ts
@@ -0,0 +1,17 @@
+const url = "https://api.clash.qwq.sh";
+
+export function createUser() {
+    return fetch(url + "/create", {
+        method: "POST",
+    }).then((res) => res.json());
+}
+
+export function getAllUsers() {
+    return fetch(url + "/all").then((res) => res.json());
+}
+
+export function submitScore(id: string, type: string) {
+    return fetch(url + `/submit/${id}/${type}`, {
+        method: "POST",
+    }).then((res) => res.json());
+}
