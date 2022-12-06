453e2080914fb258a0badd6084deb9862a61f633
angelsflyinhell <azami@namespace.media>
2022-12-06 21:54:02 +0100

    Yea components

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,37 +1,67 @@
-<script setup lang="ts">
-import router from '@/router';
+<script lang="ts">
 import { getField } from '@/utils/api';
-import FieldVue from '../components/Field.vue'
+import { defineComponent } from 'vue';
+import router from '@/router';
 
-const id = router.currentRoute.value.params.id as string;
-const field = parseInt((await getField(id)).position);
-
-const fieldData: boolean[] = new Array(9).fill(false)
-fieldData[field] = true
+export default defineComponent({
+  data() {
+    return {
+      fieldData: new Array(8).fill(false),
+      id: ""
+    }
+  },
+  methods: {
+    async getData(fieldData: boolean[], id: any) {
+      const field = (await getField(id)).position;
+      fieldData[field] = true;
+      this.fieldData = fieldData;
+    }
+  },
+  created() {
+    const id = router.currentRoute.value.params.id as string;
+    this.getData(this.fieldData, id);
+  }
+})
 </script>
 
 <template>
   <h1 class="text-4xl font-bold text-white top-0 p-4">{{ id }}</h1>
   <div class="flex flex-col h-5/6">
     <div class="flex flex-row h-1/4 p-8 w-screen">
-      <FieldVue :active="fieldData[0]" />
+      <div class="flex justify-center items-center w-2/4 bg-secondary rounded-md shadow-md">
+        <div v-if="fieldData[0]" class="h-12 w-6 rounded-full bg-error shadow-md" />
+      </div>
       <div class="h-full w-1 bg-white" />
-      <FieldVue :active="fieldData[1]" />
+      <div class="flex justify-center items-center w-2/4 bg-secondary rounded-md shadow-md">
+        <div v-if="fieldData[1]" class="h-12 w-6 rounded-full bg-error shadow-md" />
+      </div>
     </div>
     <div class="flex flex-row h-1/4 p-8">
-      <FieldVue :active="fieldData[2]" />
+      <div class="flex justify-center items-center w-2/4 bg-secondary rounded-md shadow-md">
+        <div v-if="fieldData[2]" class="h-12 w-6 rounded-full bg-error shadow-md" />
+      </div>
       <div class="h-full w-1 bg-white" />
-      <FieldVue :active="fieldData[3]" />
+      <div class="flex justify-center items-center w-2/4 bg-secondary rounded-md shadow-md">
+        <div v-if="fieldData[3]" class="h-12 w-6 rounded-full bg-error shadow-md" />
+      </div>
     </div>
     <div class="flex flex-row h-1/4 p-8">
-      <FieldVue :active="fieldData[4]" />
+      <div class="flex justify-center items-center w-2/4 bg-secondary rounded-md shadow-md">
+        <div v-if="fieldData[4]" class="h-12 w-6 rounded-full bg-error shadow-md" />
+      </div>
       <div class="h-full w-1 bg-white" />
-      <FieldVue :active="fieldData[5]" />
+      <div class="flex justify-center items-center w-2/4 bg-secondary rounded-md shadow-md">
+        <div v-if="fieldData[5]" class="h-12 w-6 rounded-full bg-error shadow-md" />
+      </div>
     </div>
     <div class="flex flex-row h-1/4 p-8">
-      <FieldVue :active="fieldData[6]" />
+      <div class="flex justify-center items-center w-2/4 bg-secondary rounded-md shadow-md">
+        <div v-if="fieldData[6]" class="h-12 w-6 rounded-full bg-error shadow-md" />
+      </div>
       <div class="h-full w-1 bg-white" />
-      <FieldVue :active="fieldData[7]" />
+      <div class="flex justify-center items-center w-2/4 bg-secondary rounded-md shadow-md">
+        <div v-if="fieldData[7]" class="h-12 w-6 rounded-full bg-error shadow-md" />
+      </div>
     </div>
   </div>
   <p class="text-white font-bold underline"><a href="https://github.com/angelsflyinhell/clash">GitHub</a></p>
