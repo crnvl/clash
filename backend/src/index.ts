8c37b07517da1dc8bea3c0bd88ff1d9eb588bcfb
angelsflyinhell <angelsflyinhell@namespace.media>
2022-12-09 21:22:31 +0100

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,6 +12,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
+    
 });
 
 
